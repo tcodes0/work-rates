ffee4d0a3b6c554dbc16defa5aa19dd6eed7f656
Raphael Thomazella <thomazella9@gmail.com>
2021-03-24 00:18:27 -0300

    chore(misc): script

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(22)
+const empty = new Array(130).fill(0);
+const base = empty.map((x, i) => (i + 2) * 5000).reverse();
+const formatted = base.map((year) => ({
+  Hourly: (year / 1920.1755589082431).toFixed(0),
+  Yearly: `${year / 1000}k`,
+  Monthly: `${parseFloat((year / 12000).toFixed(1))}k`,
+}));
+console.table(formatted);
