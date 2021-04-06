e7153fe62e58abf0a5c4e8f06dfade1451e8d77c
Raphael Thomazella <thomazella9@gmail.com>
2021-04-06 13:53:21 -0300

    chore(misc): move vars to printTable

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 #! /usr/bin/env node
 
-const empty = new Array(130).fill(0);
-const base = empty.map((x, i) => (i + 2) * 5000).reverse();
-const formatted = base.map((year) => ({
-  Hourly: (year / 1920.1755589082431).toFixed(0),
-  Yearly: `${year / 1000}k`,
-  Monthly: `${parseFloat((year / 12000).toFixed(1))}k`,
-}));
-
 function hourlyToYearly(hourly) {
   return hourly * 1920.1755589082431;
 }
@@ -33,6 +25,13 @@ function yearlyToMonthly(yearly) {
 }
 
 function printTable() {
+  const empty = new Array(130).fill(0);
+  const base = empty.map((x, i) => (i + 2) * 5000).reverse();
+  const formatted = base.map((year) => ({
+    Hourly: (year / 1920.1755589082431).toFixed(0),
+    Yearly: `${year / 1000}k`,
+    Monthly: `${parseFloat((year / 12000).toFixed(1))}k`,
+  }));
   console.table(formatted);
 }
 
