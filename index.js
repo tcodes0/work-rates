61cdfb3c07b0b1af8a085aa39f65f2414bfc5a25
Thom <thomazella9@gmail.com>
2021-04-13 11:04:35 -0300

    Add Conversion Exports, Refactor to PrintTable (#3)
    * chore(misc): add exports
    
    * chore(misc): move vars to printTable
    
    * chore(misc): add side-effects.js

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 #! /usr/bin/env node
 
-const empty = new Array(130).fill(0);
-const base = empty.map((x, i) => (i + 2) * 5000).reverse();
-const formatted = base.map((year) => ({
-  Hourly: (year / 1920.1755589082431).toFixed(0),
-  Yearly: `${year / 1000}k`,
-  Monthly: `${parseFloat((year / 12000).toFixed(1))}k`,
-}));
-console.table(formatted);
+function hourlyToYearly(hourly) {
+  return hourly * 1920.1755589082431;
+}
+
+function hourlyToMonthly(hourly) {
+  return hourly * 160;
+}
+
+function monthlyToHourly(monthly) {
+  return monthly / 160;
+}
+
+function monthlyToYearly(monthly) {
+  return monthly * 12;
+}
+
+function yearlyToHourly(yearly) {
+  return yearly / 1920.1755589082431;
+}
+
+function yearlyToMonthly(yearly) {
+  return yearly / 12;
+}
+
+function printTable() {
+  const empty = new Array(130).fill(0);
+  const base = empty.map((x, i) => (i + 2) * 5000).reverse();
+  const formatted = base.map((year) => ({
+    Hourly: (year / 1920.1755589082431).toFixed(0),
+    Yearly: `${year / 1000}k`,
+    Monthly: `${parseFloat((year / 12000).toFixed(1))}k`,
+  }));
+  console.table(formatted);
+}
+
+module.exports = {
+  hourlyToYearly,
+  hourlyToMonthly,
+  monthlyToHourly,
+  monthlyToYearly,
+  yearlyToHourly,
+  yearlyToMonthly,
+  printTable,
+};
---
+++ side-effects.js
@@ -0,0 +1,3 @@
+import { printTable } from ".";
+
+printTable();
